d49462acae083ee16c2cef9952b588b6d0d4d485
Lorenzo Paris <paris.lorenzo90@gmail.com>
2022-02-18 10:25:14 +0000

    src/constants.rs: reduced new_twins and shares limits to 4 and 128

--- src/constants.rs
+++ src/constants.rs
@@ -3,8 +3,8 @@ use std::time::Duration;
 pub const AGENT_KEY_NAME: &str = "00";
 pub const AGENT_TWIN_NAME: &str = "#twin-0";
 pub const NEW_TWINS_SHARE_TICK_CAP: f64 = 0.75;
-pub const CONCURRENT_NEW_TWINS_LIMIT: usize = 12;
-pub const CONCURRENT_SHARES_LIMIT: usize = 512;
+pub const CONCURRENT_NEW_TWINS_LIMIT: usize = 4;
+pub const CONCURRENT_SHARES_LIMIT: usize = 128;
 pub const RESCHEDULE_DELAY: Duration = Duration::from_millis(500);
 // this should match the label max length - see PATTERN_LABEL in https://github.com/Iotic-Labs/iotic-lib-metadata
 pub const MAX_LABEL_LENGTH: usize = 128;
